e2163669a01833b6cb75f21a8be1f02ac31bd4cd
AlexsaniKY <cerafem@gmail.com>
2017-12-12 20:30:17 -0500

    added file input to the dispatcher input

--- main.cpp
+++ main.cpp
@@ -6,6 +6,26 @@ using namespace std;
 Dispatcher dis;
 
 int main() {
-dis.addToInitQueue(createnullPcb());
+
+	if (!(inputliststream = fopen(inputfile, "r"))) { // open it
+          SysErrMsg("could not open dispatch list file:", inputfile);
+          exit(2);
+    }
+
+    while (!feof(inputliststream)) {  // put processes into input_queue
+        process = createnullPcb();
+        if (fscanf(inputliststream,"%d, %d, %d, %d, %d, %d, %d, %d",
+             &(process->arrivaltime), &(process->priority),
+             &(process->remainingcputime), &(process->mbytes),
+             &(process->req.printers), &(process->req.scanners),
+             &(process->req.modems), &(process->req.cds)) != 8) {
+            free(process);
+            continue;
+        }
+        process->status = PCB_INITIALIZED;
+
+         dis.addToInitQueue(process);
+    }
+
 
 }
