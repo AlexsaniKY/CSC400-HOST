b17032162393e0d55f557158b9d2c16c26048e5b
AlexsaniKY <cerafem@gmail.com>
2017-12-14 17:35:06 -0500

    reordered Dispatcher header

--- Dispatcher.h
+++ Dispatcher.h
@@ -7,9 +7,11 @@ class Dispatcher
 {
 private:
 	enum queue{RT, US1, US2, US3};
-	PcbPtr[4] job_queues = {NULL, NULL, NULL, NULL};
+	
 	PcbPtr input_queue = NULL;
 	PcbPtr new_queue = NULL;
+	PcbPtr[4] job_queues = {NULL, NULL, NULL, NULL};
+
 	Rsrc available_resources = {4,4,4,4};
 	unsigned long systime;
 	void printQueue(PcbPtr Queue);
