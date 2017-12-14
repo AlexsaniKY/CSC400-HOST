987878fa20f66115c1f8cb93aaa07c28acaae78e
AlexsaniKY <cerafem@gmail.com>
2017-12-14 17:24:51 -0500

    commenting to Dispatcher code, print queue debug code

--- Dispatcher.cpp
+++ Dispatcher.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 Dispatcher::Dispatcher()
 {
+	systime = 0;
 }
 
 
@@ -12,26 +13,61 @@ Dispatcher::~Dispatcher()
 {
 }
 
+//add process to init queue.  Jobs that are queued but not arrived will be in this queue.
+//Jobs are required to arrive in arrival order to have defined behavior.
 void Dispatcher::addToInitQueue(PcbPtr process){
 	printPcb(process, stdout);
 	input_queue = enqPcb(input_queue, process);
 }
 
+//Queues up all jobs from the New Process Queue into the appropriate RT/User feedback Queues
+//provided the resources required by the process is available.
 void Dispatcher::queueJobs(){
 }
 
-void Dispatcher::initializeProcesses(){}
+//Initializes Processes into the New Process Queue if they have "arrived"
+void Dispatcher::initializeProcesses(){
+	PcbPtr p;
+	//while input queue not empty and process not scheduled for future
+	while(input_queue && input_queue->arrivaltime <= systime){
+		//remove from input queue
+		p = deqPcb(input_queue);
+		//add to new process queue
+		new_queue = enqPcb(new_queue, p)
+	}
+}
 
+//Whether the Dispatcher still has jobs, whether initialized or not
 bool Dispatcher::hasJobs(){return false;}
 
-void Dispatcher::printInitQueue(){
-	PcbPtr p = input_queue;
+//test method to print the queue of processes not yet prepared into the ready state
+void Dispatcher::printQueue(PcbPtr Queue){
+	PcbPtr p = Queue;
 	while(p) {
 		printPcb(p, stdout);
 		p = p->next;
 		}
 }
 
+void Dispatcher::printAllQueues(){
+	cout << "Process not initialized:" << endl;
+	printQueue(input_queue);
+	cout << "Real Time Processes:" << endl;
+	printQueue(job_queues[RT]);
+	cout << "User Processes Priority 1:" << endl;
+	printQueue(job_queues[US1]);
+	cout << "User Processes Priority 2:" << endl;
+	printQueue(job_queues[US2]);
+	cout << "User Processes Priority 3:" << endl;
+	printQueue(job_queues[US3]);
+	cout << endl;
+}
+
+//Run the Dispatcher. Available processes will be scheduled, ran,
+//and afterwards if necessary, preempted and requeued.
+//
+//Notable exceptions: Real Time Processes will run to completion.
+//User Processes will not preempt until other processes exist in the job queue.
 void Dispatcher::run(){
 	
 }
--- Dispatcher.h
+++ Dispatcher.h
@@ -6,15 +6,20 @@
 class Dispatcher
 {
 private:
-	PcbPtr input_queue;
+	enum queue{RT, US1, US2, US3};
+	PcbPtr[4] job_queues = {NULL, NULL, NULL, NULL};
+	PcbPtr input_queue = NULL;
+	PcbPtr new_queue = NULL;
+	Rsrc available_resources = {4,4,4,4};
 	unsigned long systime;
+	void printQueue(PcbPtr Queue);
 public:
 	Dispatcher();
 	~Dispatcher();
 	void addToInitQueue(PcbPtr process);
 	void queueJobs();
 	void initializeProcesses();
-	void printInitQueue();
+	void printAllQueues();
 	bool hasJobs();
 	void run();
 };
